0e92cfd019e5b7438053e38486b9f3c602b856c6
Tmthang1601 <21130537@st.hcmuaf.edu.vn>
2023-11-19 04:15:19 +0700

    done fixing [workspaceId

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
-
+import { SubscriptionModalProvider } from '@/lib/providers/subscription-modal-provider';
+import { getActiveProductsWithPrice } from '@/lib/supabase/queries';
 import React from 'react';
 
 interface LayoutProps {
@@ -7,10 +8,13 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
-  
+  const { data: products, error } = await getActiveProductsWithPrice();
+  if (error) throw new Error();
   return (
     <main className="flex over-hidden h-screen">
+      <SubscriptionModalProvider products={products}>
         {children}
+      </SubscriptionModalProvider>
     </main>
   );
 };
